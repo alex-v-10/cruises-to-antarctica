5c1c600211b4ee31acfb9ed1c955d1aff2ae244b
alex-v-10 <alex.ru9311@gmail.com>
2023-04-09 02:48:13 +0300

    make footer

--- source/js/modules/menu-burger.js
+++ source/js/modules/menu-burger.js
@@ -1,5 +1,8 @@
+import {ScrollLock} from '../utils/scroll-lock';
+
 const burgerContainer = document.querySelector('[data-burger-container]');
 const burgerButton = document.querySelector('[data-burger-button]');
+const scrollLock = new ScrollLock();
 
 const initMenuBurger = () => {
   if (burgerContainer && burgerButton) {
@@ -7,6 +10,12 @@ const initMenuBurger = () => {
 
     burgerButton.addEventListener('click', () => {
       burgerContainer.classList.toggle('is-active');
+      if (burgerContainer.classList.contains('is-active')) {
+        scrollLock.disableScrolling();
+      } else {
+        scrollLock.enableScrolling();
+      }
+      window.scrollTo(0, 0);
     });
   }
 };
