2866f5c3f23710eee22be58db79fa51f6918013f
alex-v-10 <alex.ru9311@gmail.com>
2023-04-13 14:24:37 +0300

    Update 2023-04-13T11:24:30.490Z

--- source/js/modules/no-js.js
+++
@@ -1,9 +0,0 @@
-const noJsElements = document.querySelectorAll('.no-js');
-
-export const initNoJs = () => {
-  if (noJsElements.length) {
-    for (const element of noJsElements) {
-      element.classList.remove('no-js');
-    }
-  }
-};
