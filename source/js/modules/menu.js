b9a31f6ab97115d28b04e7aafc2de2613268190a
alex-v-10 <alex.ru9311@gmail.com>
2023-04-13 21:36:23 +0300

    fix burger menu

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -2,21 +2,44 @@ import {ScrollLock} from '../utils/scroll-lock';
 
 const burgerContainer = document.querySelector('[data-burger-container]');
 const burgerButton = document.querySelector('[data-burger-button]');
+const links = burgerContainer.querySelectorAll('a');
 const scrollLock = new ScrollLock();
+
+const activate = () => {
+  burgerContainer.classList.add('is-active');
+  scrollLock.disableScrolling();
+  burgerButton.removeEventListener('click', onButtonCLick);
+  document.body.addEventListener('click', onBodyClick);
+  window.scrollTo(0, 0);
+};
+
+const deactivate = () => {
+  burgerContainer.classList.remove('is-active');
+  scrollLock.enableScrolling();
+  document.body.removeEventListener('click', onBodyClick);
+  burgerButton.addEventListener('click', onButtonCLick);
+};
+
+const onButtonCLick = (event) => {
+  activate();
+  event.stopPropagation();
+};
+
+const onBodyClick = (event) => {
+  const isButton = burgerButton.contains(event.target);
+  const isLink = Array.from(links).some((item) => (item.contains(event.target)));
+  if (isButton || isLink) {
+    deactivate();
+  }
+  if (!burgerContainer.contains(event.target)) {
+    deactivate();
+  }
+};
 
 const initMenu = () => {
   if (burgerContainer && burgerButton) {
     burgerContainer.classList.remove('is-active');
-
-    burgerButton.addEventListener('click', () => {
-      burgerContainer.classList.toggle('is-active');
-      if (burgerContainer.classList.contains('is-active')) {
-        scrollLock.disableScrolling();
-      } else {
-        scrollLock.enableScrolling();
-      }
-      window.scrollTo(0, 0);
-    });
+    burgerButton.addEventListener('click', onButtonCLick);
   }
 };
 
